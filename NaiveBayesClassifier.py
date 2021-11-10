45324f6b3092c6c163559541ebcf0bc3c0e37ff9
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2021-11-09 17:39:39 -0800

    hi

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -1,25 +1,29 @@
 import math
 import pandas
 import sys
+import time
 
-columns=['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine', 'WindGustDir', 'WindGustSpeed', 'WindDir9am', 'WindDir3pm', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am',
-'Temp3pm', 'RainToday', 'RainTomorrow']
+columns = ['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine', 'WindGustDir', 'WindGustSpeed', 'WindDir9am', 'WindDir3pm', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am',
+           'Temp3pm', 'RainToday', 'RainTomorrow']
 
-categorical = ['Location','WindGustDir', 'WindDir9am', 'WindDir3pm', 'RainToday']
+categorical = ['Location', 'WindGustDir',
+               'WindDir9am', 'WindDir3pm', 'RainToday']
 categoricalIndexes = [0, 6, 8, 9, 20]
 
-numerical = ['MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine', 'WindGustSpeed', 'WindSpeed9am','WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am',
-'Temp3pm']
-numericalIndexes = [1,2,3,4,5,7,10,11,12,13,14,15,16,17,18,19]
+numerical = ['MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine', 'WindGustSpeed', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am',
+             'Temp3pm']
+numericalIndexes = [1, 2, 3, 4, 5, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
-def makeDatasets(train,test):
+
+def makeDatasets(train, test):
     train_dataset = pandas.read_csv(train, sep=",", header=None)
     test_dataset = pandas.read_csv(test, sep=",", header=None)
     train_dataset = train_dataset.set_axis(labels=columns, axis=1)
     test_dataset = test_dataset.set_axis(labels=columns, axis=1)
-    train_dataset_yes = train_dataset[train_dataset['RainTomorrow'] == ' Yes'] 
-    train_dataset_no = train_dataset[train_dataset['RainTomorrow'] == ' No'] 
+    train_dataset_yes = train_dataset[train_dataset['RainTomorrow'] == ' Yes']
+    train_dataset_no = train_dataset[train_dataset['RainTomorrow'] == ' No']
     return (train_dataset_yes, train_dataset_no, test_dataset)
+
 
 def calculateProbability(x, mean, std):
     result = 1.0/(std * math.sqrt(2 * math.pi))
@@ -27,19 +31,20 @@ def calculateProbability(x, mean, std):
     result = result * math.pow(math.e, e_term)
 
     if(result < math.pow(math.e, -10)):
-        result = math.pow(math.e,-10)
+        result = math.pow(math.e, -10)
 
     return result
 
-def classifier(train_dataset1, train_dataset2, testRow):
+
+def classifier(train_dataset1, train_dataset2, testRow, my, sy, mn, sn):
     train_dataset_yes = train_dataset1
     train_dataset_no = train_dataset2
 
-    means_yes = train_dataset_yes.mean(axis=0, numeric_only=True)
-    stds_yes = train_dataset_yes.std(axis=0, numeric_only=True)
+    means_yes = my
+    stds_yes = sy
 
-    means_no = train_dataset_no.mean(axis=0, numeric_only=True)
-    stds_no = train_dataset_no.std(axis=0, numeric_only=True)
+    means_no = mn
+    stds_no = sn
 
     c_yes = math.log(1./2, 10)
     c_no = math.log(1./2, 10)
@@ -47,40 +52,42 @@ def classifier(train_dataset1, train_dataset2, testRow):
     for category in categorical:
         if(category == 'Location'):
             yes = train_dataset_yes[category].value_counts()
-            c_yes += math.log(yes[testRow[0]]/9911,10)
-            
+            c_yes += math.log(yes[testRow[0]]/9911, 10)
+
             no = train_dataset_no[category].value_counts()
-            c_no += math.log(no[testRow[0]]/9911,10)
+            c_no += math.log(no[testRow[0]]/9911, 10)
         if(category == 'WindGustDir'):
             yes = train_dataset_yes[category].value_counts()
-            c_yes += math.log(yes[testRow[6]]/9911,10)
-            
+            c_yes += math.log(yes[testRow[6]]/9911, 10)
+
             no = train_dataset_no[category].value_counts()
-            c_no += math.log(no[testRow[6]]/9911,10)
+            c_no += math.log(no[testRow[6]]/9911, 10)
         if(category == 'WindDir9am'):
             yes = train_dataset_yes[category].value_counts()
-            c_yes += math.log(yes[testRow[8]]/9911,10)
-            
+            c_yes += math.log(yes[testRow[8]]/9911, 10)
+
             no = train_dataset_no[category].value_counts()
-            c_no += math.log(no[testRow[8]]/9911,10)
+            c_no += math.log(no[testRow[8]]/9911, 10)
         if(category == 'WindDir3pm'):
             yes = train_dataset_yes[category].value_counts()
-            c_yes += math.log(yes[testRow[9]]/9911,10)
-            
+            c_yes += math.log(yes[testRow[9]]/9911, 10)
+
             no = train_dataset_no[category].value_counts()
-            c_no += math.log(no[testRow[9]]/9911,10)
+            c_no += math.log(no[testRow[9]]/9911, 10)
         if(category == 'RainToday'):
             yes = train_dataset_yes[category].value_counts()
-            c_yes += math.log(yes[testRow[20]]/9911,10)
-            
-            no = train_dataset_no[category].value_counts()
-            c_no += math.log(no[testRow[20]]/9911,10)
+            c_yes += math.log(yes[testRow[20]]/9911, 10)
 
-    i=0
+            no = train_dataset_no[category].value_counts()
+            c_no += math.log(no[testRow[20]]/9911, 10)
+
+    i = 0
     for category in numerical:
-        prob_yes = calculateProbability(testRow[numericalIndexes[i]], means_yes[category], stds_yes[category])
-        c_yes += math.log(prob_yes,10)
-        prob_no = calculateProbability(testRow[numericalIndexes[i]], means_no[category], stds_no[category])
+        prob_yes = calculateProbability(
+            testRow[numericalIndexes[i]], means_yes[category], stds_yes[category])
+        c_yes += math.log(prob_yes, 10)
+        prob_no = calculateProbability(
+            testRow[numericalIndexes[i]], means_no[category], stds_no[category])
         c_no += math.log(prob_no, 10)
         i += 1
 
@@ -89,16 +96,40 @@ def classifier(train_dataset1, train_dataset2, testRow):
     else:
         return 0
 
+
 def train_and_test():
-    datasets = makeDatasets(sys.argv[1],sys.argv[2])
+    start = time.time()
+    datasets = makeDatasets(sys.argv[1], sys.argv[2])
     train_dataset_yes = datasets[0]
     train_dataset_no = datasets[1]
     test_dataset = datasets[2]
 
-    test_dataset=test_dataset.drop(['RainTomorrow'],axis=1)
+    means_yes = train_dataset_yes.mean(axis=0, numeric_only=True)
+    stds_yes = train_dataset_yes.std(axis=0, numeric_only=True)
+
+    means_no = train_dataset_no.mean(axis=0, numeric_only=True)
+    stds_no = train_dataset_no.std(axis=0, numeric_only=True)
+
+    expected=test_dataset['RainTomorrow'].values.tolist()
+    test_dataset = test_dataset.drop(['RainTomorrow'], axis=1)
+
+    right = 0
+    total = 0
 
     for row in test_dataset.values.tolist():
-        print(classifier(train_dataset_yes, train_dataset_no, row))
+        prediction = classifier(train_dataset_yes, train_dataset_no,
+              row, means_yes, stds_yes, means_no, stds_no)
+        print(prediction)
+        # if prediction == 1 and expected[total] == ' Yes':
+        #     right +=1
+        # if prediction == 0 and expected[total] == ' No':
+        #     right +=1
+        # total += 1
     
+    end = time.time()
+    # print('Results for running naive bayes classifier:')
+    # print('Accuracy: ', right/total)
+    # print('Runtime: ', end - start)
+
 
 train_and_test()
